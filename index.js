7f301d294168e466ecca12834f66f0be8edb20df
jerodwcox-Frontend <dnldfn76@gmail.com>
2026-02-28 17:19:15 +0700

    trust in the Lord

--- index.js
+++ index.js
@@ -34,15 +34,13 @@ function contact(event) {
   const loading = document.querySelector(".modal__overlay--loading");
   const success = document.querySelector(".modal__overlay--success");
   loading.classList += " modal__overlay--visible";
-  emailjs
-    .sendForm(
-      "service_80ih0if",
-      "template_d9refyl",
-      event.target,
-      "user_K1PoFs8pB2YVWStDxrUls"
-    )
-    .then(() => {
-      loading.classList.remove("modal__overlay--visible");
+emailjs.sendForm(
+  "service_80ih0if",
+  "template_d9refyl",
+  event.target
+)
+.then(() => {
+    loading.classList.remove("modal__overlay--visible");
       success.classList += " modal__overlay--visible";
     })
     .catch(() => {
